e10ad9bf43728c9af1cd6e51343b2ebabe8dfc37
Stefan <2590472737@qq.com>
2026-04-25 10:20:35 +0800

    添加了自动部署

--- client/src/shared/api/axios.ts
+++ client/src/shared/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || "https://inkflowx.netlify.app";
+const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api/v1";;
 
 export const api = axios.create({
     baseURL,
